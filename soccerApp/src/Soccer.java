71edcda3dd93a843b241e76a53571197a976c01f
HPatel999 <tornadepatel@gmail.com>
2023-03-14 00:26:33 -0400

    Addendum to menu
    Addendum to menu

--- soccerApp/src/Soccer.java
+++ soccerApp/src/Soccer.java
@@ -3,18 +3,21 @@ import  java.util.Scanner;
 public class Soccer {
 
     public static void main(String[] args) {
-        while(true) {
-            System.out.println("Soccer Main Menu\n");
-            System.out.println("\t 1. List information of matches of a country");
-            System.out.println("\t 2. Insert initial player information for a match");
-            System.out.println("\t 3. For you to design");
-            System.out.println("\t 4. Exit Application");
-            System.out.println("Please Enter Your Option:\n");
 
+        System.out.println("Soccer Main Menu");
+        System.out.println("\t 1. List information of matches of a country");
+        System.out.println("\t 2. Insert initial player information for a match");
+        System.out.println("\t 3. For you to design");
+        System.out.println("\t 4. Exit Application");
+        System.out.println("Please Enter Your Option:\n");
+        while(true) {
 
             Scanner myInput = new Scanner(System.in);
             String optionCode = myInput.nextLine();
             System.out.println(optionCode);
+
+
+
 
         }
 
