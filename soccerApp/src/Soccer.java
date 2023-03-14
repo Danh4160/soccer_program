832b57292623a38c26b63cd41e81e02ba456fcd4
HPatel999 <tornadepatel@gmail.com>
2023-03-14 00:17:09 -0400

    Main Menu
    Maine menu while and option code

--- soccerApp/src/Soccer.java
+++ soccerApp/src/Soccer.java
@@ -1,6 +1,23 @@
+
+import  java.util.Scanner;
 public class Soccer {
 
     public static void main(String[] args) {
-        System.out.println("Hello Soccer\n");
-    }
+        while(true) {
+            System.out.println("Soccer Main Menu\n");
+            System.out.println("\t 1. List information of matches of a country");
+            System.out.println("\t 2. Insert initial player information for a match");
+            System.out.println("\t 3. For you to design");
+            System.out.println("\t 4. Exit Application");
+            System.out.println("Please Enter Your Option:\n");
+
+
+            Scanner myInput = new Scanner(System.in);
+            String optionCode = myInput.nextLine();
+            System.out.println(optionCode);
+
+        }
+
+
+        }
 }
